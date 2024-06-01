d23b9dc45adda0c0d43d9230c06f4190f53b9de3
SzpilmanODST <szpilmanv05@gmail.com>
2024-06-01 16:08:35 -0600

    project started

---
+++ src/main/java/com/InmueblesMX/InmueblesMxApplication.java
@@ -0,0 +1,13 @@
+package com.InmueblesMX;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InmueblesMxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InmueblesMxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/InmueblesMX/InmueblesMxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.InmueblesMX;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InmueblesMxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
